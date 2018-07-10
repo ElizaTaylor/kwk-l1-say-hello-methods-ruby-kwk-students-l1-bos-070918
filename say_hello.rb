fc42a1c6c5d50e062c31e6a774f3bcfacb2fcb8c
Eliza Taylor <elizataylor2002@gmail.com>
2018-07-10 15:34:00 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,9 @@
 # Build your say_hello method here
+
+def say_hello(your_name, my_name)
+  puts  "hello #{your_name}! im #{my_name}."
+end 
+
+
+say_hello("Nick","Eliza")
+say_hello("hanah","Eliza")
